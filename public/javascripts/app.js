4a6aafdb4ba5dc5acae7e18e61726ee84c570e39
Naomi Paulmin <naosup@users.noreply.github.com>
2018-04-23 19:30:43 +0200

    [Added] Upgrades

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,11 +1,36 @@
 const socket = io(); 
+var upgrade;
 
-socket.on('UserState', (data) => {
-    $('.connected-number').text(data);
-});
 socket.on('Click', (data) => {
     $('.counter').text(data);
+    if(data >= 10){
+        $('#upgrade1').prop('disabled', false);
+    }
+    if(data >= 30){
+        $('#upgrade2').prop('disabled', false);
+    }
 }); 
 $('.clicker').click(() => {
     socket.emit('Click');
 });
+
+$('#upgrade1').click(() => {
+    upgrade = 1;
+    socket.emit('Upgrade', upgrade);
+});
+
+$('#upgrade2').click(() => {
+    upgrade = 2;
+    socket.emit('Upgrade', upgrade);
+});
+
+socket.on('Upgrade', (data) => {
+    $('.counter').text(data);
+    if (data < 10) {
+        $('#upgrade1').prop('disabled', true);
+    }
+    if(data < 30){
+        $('#upgrade2').prop('disabled', true);
+    }
+});
+
--- sockets/Base.js
+++ sockets/Base.js
@@ -1,22 +1,26 @@
 module.exports = (io) => {
     var counter = 0;
-    io.on('connection', (socket) => {
-        console.log(counter);
-        console.log('+1');
-            // On envoie le nombre de personnes actuellement sur le socket à tout le monde (sauf la personne qui vient de se connecter)
-            socket.broadcast.emit('UserState', io.engine.clientsCount);
-            // On envoie le nombre de personnes actuellement sur le socket à la personne qui vient de se connecter
-            socket.emit('UserState', io.engine.clientsCount);
+    var upgrade1 = 0;
+    var upgrade2 = 0;
 
-             socket.on('disconnect', () => {
-                 console.log('-1');
-              // On prévient tout le monde qu'une personne s'est deconnectée
-                socket.broadcast.emit('UserState', io.engine.clientsCount);
-            });
+    io.on('connection', (socket) => {
+        socket.emit('Click', counter);
+        socket.on('Click', () => {
+            counter += 1;
             socket.emit('Click', counter);
-            socket.on('Click', () => {
-                counter += 1;
-                socket.emit('Click', counter);
-            })
+        });
+        socket.on('Upgrade', (upgrade) => {
+            switch(upgrade){
+                case 1:
+                    counter -= 10;
+                    upgrade1 += 1;
+                    break;
+                case 2:
+                    counter -= 30;
+                    upgrade2 += 1;
+                    break;
+            }
+            socket.emit('Upgrade', counter);
+        });
     });
 };
